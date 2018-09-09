b501960d8d4b60f088a0e977dde8465a718b066a
lgl <505827439@qq.com>
2018-09-09 10:50:30 +0800

    favicon

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ app.controller("mainCtrl", function ($scope) {
     $scope.showLogo = function () {
         layer.open({
             type: 1,
-            title: 0,
-            area: ['400px', '400px'],
+            title: "信息",
+            area: ['600px', '342px'],
             shadeClose: 1,
-            content: "<img src='./imgs/header.jpeg'>"
+            content: "<div class='row '><img class='col-sm-6' src='./imgs/header.jpeg' style='height: 100%;'><div class='col-sm-6 pull-right'><p>姓名:<span>罗国梁</span></p></div></div>"
         })
     }
 });
