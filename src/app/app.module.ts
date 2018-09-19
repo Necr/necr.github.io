d16d0efca1d3e5df90ce20cb275fe12273080c0a
lgl <505827439@qq.com>
2018-09-19 19:29:24 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SwiperComponent } from './swiper/swiper.component';
+import {NgProgressModule} from "ngx-progressbar";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SwiperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
